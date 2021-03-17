385ae0f0892aeabe382fd4f52ec71a9a3bad66c8
bozimmerman <bo@zimmers.net>
2021-03-17 05:18:15 +0000

    added missing eoln
    git-svn-id: svn://192.168.1.10/public/Zimodem@20637 0d6f1817-ed0e-0410-87c9-987e46238f29

--- zimodem/zcommand.ino
+++ zimodem/zcommand.ino
@@ -764,7 +764,7 @@ ZResult ZCommand::doInfoCommand(int vval, uint8_t *vbuf, int vlen, bool isNumber
   }
   case 10:
   {
-    serial.printf("%s",printMode.getLastPrinterSpec());
+    serial.printf("%s%s",printMode.getLastPrinterSpec(),EOLN.c_str());
     break;
   }
   case 11:
