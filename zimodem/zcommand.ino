c01786505ec34465cb7fed1d2dccb1ad77e75647
James Denton <james.denton@outlook.com>
2021-03-22 22:15:57 -0500

    ATI10 shows printer spec

--- zimodem/zcommand.ino
+++ zimodem/zcommand.ino
@@ -764,6 +764,11 @@ ZResult ZCommand::doInfoCommand(int vval, uint8_t *vbuf, int vlen, bool isNumber
   }
   case 10:
   {
+    serial.printf("%s",printMode.getLastPrinterSpec());
+    break;
+  }
+  case 11:
+  {
     serial.printf("%d%s",ESP.getFreeHeap(),EOLN.c_str());
     break;
   }
